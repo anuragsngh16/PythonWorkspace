3ae87b5028f839dac0cbb834e4abfadc1329b151
anurag <anuragsngh16@gmail.com>
2021-09-16 19:25:54 +0530

    Improved the code

--- DaysAndYear.py
+++ DaysAndYear.py
@@ -1,8 +1,10 @@
 travel_per_day = int(input("How many Kms can u travel in a day!"))
-print(travel_per_day)
-days_to_cover_10K_Km = 10_000 / travel_per_day
-months_to_cover_10K_Km = days_to_cover_10K_Km/30
-year_to_cover_10K_Km = months_to_cover_10K_Km/12
-print("No of days to cover 10_000 Km is: ", days_to_cover_10K_Km)
-print("No of months to cover 10_000 Km is: ", months_to_cover_10K_Km)
-print("No of years to cover 10_000 Km is: ", year_to_cover_10K_Km)
+
+days_to_cover_10K_Km = int(10_000 / travel_per_day)
+year = int(days_to_cover_10K_Km/365)
+month = int((days_to_cover_10K_Km - year*365)/30)
+days = int((days_to_cover_10K_Km - year * 365 - month*30))
+
+print("No of days to cover 10_000 Km is: ", days)
+print("No of months to cover 10_000 Km is: ", month)
+print("No of years to cover 10_000 Km is: ", year)
